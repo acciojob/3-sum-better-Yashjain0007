8069218c3c2b4e651f949256d6e8e674aba9468c
Yashjain0007 <124593014+Yashjain0007@users.noreply.github.com>
2023-03-17 19:57:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function threeSum(arr, target) {
 // write your code here
+	arr.sort(function(a,b){return a-b;});
+	let sum =0;
+	for(let i=0; i<arr.length; i++)
+		{
+			sum = arr[target] + arr[target-1] + arr[target+1];
+		}
+	return sum;
   
 }
 
